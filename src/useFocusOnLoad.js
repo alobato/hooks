dae5a58c24964f25e5f0236140211405e8b95a6c
Arley Lobato <arleylobato@gmail.com>
2022-05-06 21:40:54 -0300

    Add useFocusOnLoad

---
+++ src/useFocusOnLoad.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+
+function useFocusOnLoad(elementName, select = false) {
+  useEffect(() => {
+    const isMobile = Boolean(navigator.userAgent.match(/Android/i)) || Boolean(navigator.userAgent.match(/iPhone|iPad|iPod/i))
+    if (!isMobile) {
+      setTimeout(() => {
+        const firstElement = document.querySelector(`[name="${elementName}"]`)
+        if (firstElement) {
+          firstElement.focus()
+          if (select) firstElement.select()
+        }
+      }, 100)
+    }
+  }, [elementName, select])
+}
+
+export default useFocusOnLoad
